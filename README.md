543459788a7c846c7d0838f926c476472dbc38b3
dedkot <dedkot01@gmail.com>
2024-04-19 22:26:23 +0300

    add all crud for meeting room

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # room_reservation
 
+Учебный проект в рамках прохождения курса "FastAPI. Расширенный курс":
+
+https://practicum.yandex.ru/trainer/b2b-python-fastapi
+
 **Table of Contents**
 
 - [Installation](#installation)
--- room_reservation/schemas/meeting_room.py
+++ room_reservation/schemas/meeting_room.py
@@ -1,6 +1,27 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 
 
-class MeetingRoomCreate(BaseModel):
-    name: str = Field(..., min_length=1, max_length=100)
+class MeetingRoomBase(BaseModel):
+    name: str | None = Field(None, min_length=1, max_length=100)
     description: str | None
+
+
+class MeetingRoomCreate(MeetingRoomBase):
+    name: str = Field(..., min_length=1, max_length=100)
+
+
+class MeetingRoomUpdate(MeetingRoomBase):
+    pass
+
+    @validator('name')
+    def name_cannot_be_null(cls, value):
+        if value is None:
+            raise ValueError('Имя переговорки не может быть пустым!')
+        return value
+
+
+class MeetingRoomDB(MeetingRoomCreate):
+    id: int
+
+    class Config:
+        orm_mode = True
